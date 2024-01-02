52cabe46147bc2b12b22b6b0c928d7d85042cd3e
Alexander Reinhold <mail@alexander-reinhold.de>
2024-01-02 14:54:48 +0100

    Basic verification in Security module implemented. Some important verification is missing.

--- src/kiss_cf/security/security.py
+++ src/kiss_cf/security/security.py
@@ -40,7 +40,7 @@ class Security():
 
     def __init__(self,
                  salt: str,
-                 file: str = './data/security/user.keys'):
+                 file: str = './data/security/keys'):
         '''Get security context.
 
         The salt used to generate secret keys from password is set with
@@ -50,35 +50,35 @@ class Security():
         self._salt = salt
         self._file = file
         self._derived_key = b''
-        self.key_dict = _get_default_key_dict()
+        self._key_dict = _get_default_key_dict()
 
-    def __write_keys(self):
+    def _write_keys(self):
         '''Write key_dict to encrypted file
 
         Encryption is based on user's password (derived key)
         '''
-        data = pickle.dumps(self.key_dict)
-        self.__encrypt_to_file(self._derived_key, data, self._file)
+        data = pickle.dumps(self._key_dict)
+        self._encrypt_to_file(self._derived_key, data, self._file)
 
-    def __verify_version(self):
+    def _verify_version(self):
         ''' Verify correct version
 
         This code might catch later version adaptions.
         '''
-        if 'version' not in self.key_dict.keys():
+        if 'version' not in self._key_dict.keys():
             raise KissSecurityException('Not a KISS security file: no version information')
-        if self.key_dict['version'] != 1:
+        if self._key_dict['version'] != 1:
             raise KissSecurityException(
-                f'Keys stored in version {self.key_dict["version"]}, expected is version 1')
+                f'Keys stored in version {self._key_dict["version"]}, expected is version 1')
 
-    def __load_keys(self):
+    def _load_keys(self):
         '''Read key_dict from encrypted file
 
         Encryption is based on user's password (derived key)
         '''
-        data = self.__decrypt_from_file(self._derived_key, self._file)
-        self.key_dict = pickle.loads(data)
-        self.__verify_version()
+        data = self._decrypt_from_file(self._derived_key, self._file)
+        self._key_dict = pickle.loads(data)
+        self._verify_version()
 
     def is_user_initialized(self):
         '''Check if user secret key is initialized.
@@ -95,7 +95,7 @@ class Security():
         You can use the GUI from login.Login to unlock a user or you use
         Security.unlock_user() directly.
         '''
-        return bool(self.key_dict['symmetric_key'])
+        return bool(self._key_dict['symmetric_key'])
 
     def init_user(self, password):
         '''Initialize user secret key.
@@ -111,9 +111,9 @@ class Security():
         # Do not overwrite existing keys:
         if self.is_user_initialized():
             raise KissSecurityException('Keys are already initialized.')
-        self._derived_key = self.__derive_key(password)
-        self.key_dict['symmetric_key'] = self.__generate_key()
-        self.__write_keys()
+        self._derived_key = self._derive_key(password)
+        self._key_dict['symmetric_key'] = self._generate_key()
+        self._write_keys()
 
     def unlock_user(self, password):
         '''Unlock encryp/decrypt for user context by password.
@@ -126,23 +126,23 @@ class Security():
             raise Exception(
                 'User is not initialized. Run init_user() '
                 f'if file {self._file} was lost.')
-        self._derived_key = self.__derive_key(password)
-        self.__load_keys()
+        self._derived_key = self._derive_key(password)
+        self._load_keys()
 
     def _get_symmetric_key(self):
         if not self.is_user_unlocked():
             raise Exception(
                 f'Trying to encrypt {self.file} before '
                 'succeeding with unlock_user()')
-        return self.key_dict['symmetric_key']
+        return self._key_dict['symmetric_key']
 
     def encrypt_to_file(self, data, file):
-        self.__encrypt_to_file(self._get_symmetric_key(), data, file)
+        self._encrypt_to_file(self._get_symmetric_key(), data, file)
 
     def encrypt_to_bytes(self, data) -> bytes:
-        return self.__encrypt_to_bytes(self._get_symmetric_key(), data)
+        return self._encrypt_to_bytes(self._get_symmetric_key(), data)
 
-    def __encrypt_to_file(self, key, data, file):
+    def _encrypt_to_file(self, key, data, file):
         # get file where file is allowed to be a list of strings to avoid
         # caller needing to use os.path:
         if not isinstance(file, list):
@@ -154,27 +154,27 @@ class Security():
             os.makedirs(file_dir)
 
         with open(file, 'wb') as f:
-            f.write(self.__encrypt_to_bytes(key, data))
+            f.write(self._encrypt_to_bytes(key, data))
 
-    def __encrypt_to_bytes(self, key, data) -> bytes:
+    def _encrypt_to_bytes(self, key, data) -> bytes:
         return Fernet(key).encrypt(data)
 
     def decrypt_from_file(self, file) -> bytes:
-        return self.__decrypt_from_file(self._get_symmetric_key(), file)
+        return self._decrypt_from_file(self._get_symmetric_key(), file)
 
     def decrypt_from_bytes(self, data: bytes) -> bytes:
-        return self.__decrypt_from_bytes(self._get_symmetric_key(), data)
+        return self._decrypt_from_bytes(self._get_symmetric_key(), data)
 
-    def __decrypt_from_file(self, key, file) -> bytes:
+    def _decrypt_from_file(self, key, file) -> bytes:
         if not isinstance(file, list):
             file = [file]
         # read encrypted data
         with open(os.path.join(*file), 'rb') as f:
             data_encrypted = f.read()
 
-        return self.__decrypt_from_bytes(key, data_encrypted)
+        return self._decrypt_from_bytes(key, data_encrypted)
 
-    def __decrypt_from_bytes(self, key, data: bytes) -> bytes:
+    def _decrypt_from_bytes(self, key, data: bytes) -> bytes:
         # Note that Fernet will also validate the data on decryption. If the
         # algorithm is changed, it needs to be ensured that the decryption is
         # validated before returning it back to the caller (like using a hash
@@ -182,22 +182,49 @@ class Security():
         return Fernet(key).decrypt(data)
 
     def get_validation_public_key(self) -> bytes:
-        return b'validation_public_key'
-        # TODO UPGRADE: implement
         if not self.is_user_unlocked():
             raise Exception(
                 'Trying to access public key before '
                 'succeeding with unlock_user()')
-        if not self.__rsa_keys_exist():
-            self.__generate_rsa_keys()
-
-        return self.decrypt_from_file(self._public_key_file)
+        self._ensure_validation_keys_exist()
+        return self._key_dict['validation_pub_key']
 
     def get_encryption_public_key(self) -> bytes:
-        return b'encrption_public_key'
-        # TODO UPGRADE: implement
+        if not self.is_user_unlocked():
+            raise Exception(
+                'Trying to access public key before '
+                'succeeding with unlock_user()')
+        self._ensure_encryption_keys_exist()
+        return self._key_dict['encryption_pub_key']
 
-    def __generate_rsa_keys(self):
+    def _ensure_validation_keys_exist(self):
+        ''' Create keys, if required '''
+        if (not self._key_dict['validation_pub_key'] and
+            not self._key_dict['validation_priv_key']):
+            self._generate_validation_keys()
+            return
+        if (not self._key_dict['validation_pub_key'] or
+            not self._key_dict['validation_priv_key']):
+            raise KissSecurityException(
+                'Only public or private validation key are set. '
+                'This should not happen.'
+                )
+
+    def _ensure_encryption_keys_exist(self):
+        ''' Create keys, if required '''
+        if (not self._key_dict['encryption_pub_key'] and
+            not self._key_dict['encryption_priv_key']):
+            self._generate_encryption_keys()
+            return
+        if (not self._key_dict['encryption_pub_key'] or
+            not self._key_dict['encryption_priv_key']):
+            raise KissSecurityException(
+                'Only public or private encryption key are set. '
+                'This should not happen.'
+                )
+
+    def _generate_validation_keys(self):
+        ''' Add validation keys '''
         key = rsa.generate_private_key(
             public_exponent=65537,
             key_size=2048)
@@ -206,21 +233,59 @@ class Security():
             format=serialization.PrivateFormat.PKCS8,
             encryption_algorithm=serialization.NoEncryption()
             )
-        self.encrypt_to_file(private_key, self._private_key_file)
         public_key = key.public_key().public_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.PKCS1)
-        self.encrypt_to_file(public_key, self._public_key_file)
+        self._key_dict['validation_pub_key'] = public_key
+        self._key_dict['validation_priv_key'] = private_key
+        self._write_keys()
+
+    def _generate_encryption_keys(self):
+        ''' Add validation keys '''
+        # TODO UPGRADE: Should there be different key variants/algorithms??
+        key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048)
+        private_key = Security._serialize_private_key(key)
+        public_key = Security._serialize_private_key(key)
+        self._key_dict['encryption_pub_key'] = public_key
+        self._key_dict['encryption_priv_key'] = private_key
+        self._write_keys()
+
+    @classmethod
+    def _serialize_public_key(cls, key: rsa.RSAPublicKey) -> bytes:
+        return key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.PKCS1)
+    @classmethod
+    def _deserialize_public_key(cls, key_bytes: bytes) -> rsa.RSAPublicKey:
+        key = serialization.load_pem_public_key(key_bytes)
+        if not isinstance(key, rsa.RSAPublicKey):
+            raise KissSecurityException(
+                f'Unexpected key class {key.__class__.__name__}. '
+                f'Expected RSAPrivateKey.')
+        return key
+    @classmethod
+    def _serialize_private_key(cls, key: rsa.RSAPrivateKey) -> bytes:
+        return key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption())
+    @classmethod
+    def _deserialize_private_key(cls, key_bytes: bytes) -> rsa.RSAPrivateKey:
+        key = serialization.load_pem_private_key(key_bytes, password=None)
+        if not isinstance(key, rsa.RSAPrivateKey):
+            raise KissSecurityException(
+                f'Unexpected key class {key.__class__.__name__}. '
+                f'Expected RSAPrivateKey.')
+        return key
 
     def sign(self, data: bytes) -> bytes:
-        return b'a signature'
-        # TODO UPGRADE: redo implementation
         '''Sign a data byte stream'''
-        self.__ensure_rsa_keys_exist()
-        private_key = serialization.load_pem_private_key(
-            self.decrypt_from_file(self._private_key_file),
-            password=None
-            )
+        self._ensure_validation_keys_exist()
+        private_key = Security._deserialize_private_key(
+            self._key_dict['validation_priv_key'])
+
         return private_key.sign(
             data,
             padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
@@ -228,17 +293,14 @@ class Security():
                         ),
             hashes.SHA256())
 
+    # TODO: verification might not require a login and functionality should be
+    # available as class function.
     def verify(self, data, signature, public_key_bytes=None) -> bool:
-        return bool(signature == 'a signature')
-        # TODO UPGRADE: implementation
-        if not self.__rsa_keys_exist():
-            raise Exception(
-                'Trying to verify data while no private/public key exists. '
-                'Key files are lost or were never generated.')
-        if public_key_bytes is None:
-            public_key_bytes = self.decrypt_from_file(self._public_key_file)
-        public_key = serialization.load_pem_public_key(
-            public_key_bytes)
+        if not public_key_bytes:
+            self._ensure_validation_keys_exist()
+            public_key_bytes = self._key_dict['validation_pub_key']
+        public_key = Security._deserialize_public_key(public_key_bytes)
+
         try:
             public_key.verify(
                 signature, data,
@@ -249,7 +311,7 @@ class Security():
         except InvalidSignature:
             return False
 
-    def __derive_key(self, pwd):
+    def _derive_key(self, pwd):
         '''Derive key from password.
 
         This should be done immediately after getting a password. The password
@@ -266,7 +328,7 @@ class Security():
                 )
         return base64.urlsafe_b64encode(kdf.derive(bytes(pwd, 'utf-8')))
 
-    def __generate_key(self):
+    def _generate_key(self):
         '''Generate key to encrypt/decrypt synchronously.
 
         As of now, this just forwards to Fernet's generate_key(). See here:
