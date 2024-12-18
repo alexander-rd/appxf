a04ca2e3a3de193facd6c90c2b82ebc54a31b773
alexander-nbg <mail@alexander-nbg.de>
2024-12-18 23:37:31 +0100

    Initial checkin for selectable settings. Select is fixed on strings as a first attempt. Added a manual test case to maintain a user perspective.

---
+++ src/kiss_cf/setting/setting_select.py
@@ -0,0 +1,79 @@
+''' Settings selecting a value from a predefined list '''
+from .setting import AppxfSetting, AppxfString
+from typing import NewType
+
+# Intent is to support complex data like long text templates by selecting and
+# referencing it by a title.
+#
+# The following differences apply to standard settings:
+#   * Input is a string from a predefined list (drop down, no reason for other
+#     types since it's purpose is "name for the setting value")
+#   * It will be uncommon or not even possible to set the value directly
+#
+# A further extention is a configurable and storable selection. For standard
+# settings, the application only knows the type of the setting. Select settings
+# will have a changeable list of options which can be stored along or separate
+# to the setting.
+
+# TODO: should the custom type be the "storable" selection or at least
+# something that supports the serialization?
+
+# Simple Use Case: Text Inspection.
+#
+# The tool provides a set of Email texts that can be displayed by a list
+# select. The select would be too long to reference the whole text such that
+# only a title appears in the list.
+
+# Storable Use Case: Storable Email Templates
+#
+# The tool user can store Email text templates into the configuration for
+# reuse.
+
+# Storable Focus Use Case: Translations
+#
+# The transtlation keywords (list selection keys) are fixed but the user can
+# edit the translations (the values behind an AppxfStringSelection). The tool
+# would use use the AppxfStringSelect conversions.
+
+# Given from the above examples, there is different behavior that may or may
+# not be possible:
+#   1) Adding or removing new selectable items
+#   2) Changing the value behind a selectable item
+#
+# If neither (1) or (2) is possible, the selection list is known at
+# construction time and no storage is necessary. Only (1) possible is not
+# reasonable, but only (2) has valid use cases.
+
+AppxfSelect = NewType('AppxfSelect', object)
+
+# A precondition for string select is an editable mapping
+class AppxfStringSelect(AppxfSetting[dict]):
+    def __init__(self, key_type=str, value_type=str):
+        pass
+# TODO: the above will be mainly motivated by the way an AppxfStringSelect
+# should be edited.
+
+class AppxfStringSelect(AppxfSetting[str]):
+    ''''''
+    def __init__(self, value: str | None = None, name: str = '', **kwargs):
+        super().__init__(value, name, **kwargs)
+    #def __init__(self, options: dict | None = None):
+    #    if options is None:
+    #        options = {}
+    #    self._options = options
+
+    @classmethod
+    def get_supported_types(cls) -> list[type | str]:
+        return [AppxfSelect, 'StringSelect']
+
+    def _validate_base_type(self, value: str) -> bool:
+        return False
+    # TODO: Problem is that despite string input, just verifying input is not
+    # sufficient.
+
+    def _validated_conversion(self, value: Any) -> tuple[bool, AppxfSelect]:
+        pass
+
+    @classmethod
+    def get_default(cls):
+        return ''
