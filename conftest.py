506a716187d0af26c305759d4cac1d3c555842f7
alexander-nbg <mail@alexander-nbg.de>
2025-01-02 23:59:46 +0100

    Fix windows test execution (conftest logging setup issue)

--- conftest.py
+++ conftest.py
@@ -3,5 +3,7 @@ from appxf import logging
 
 FLAG_LOG_ACTIVATED = False
 def pytest_runtest_setup(item):
+    global FLAG_LOG_ACTIVATED
     if not FLAG_LOG_ACTIVATED:
         logging.activate_logging('kiss_cf')
+        FLAG_LOG_ACTIVATED = True
