6e260ebfa471a3cbac5174084287297164c198b9
alexander-nbg <mail@alexander-nbg.de>
2025-01-20 10:22:25 +0100

    Stateful 100% covered.

--- tests/test_statful.py
+++ tests/test_statful.py
@@ -23,9 +23,8 @@ def test_predefined_default():
     assert 'state_list' in state
     assert len(state) == 1
     assert state['state_list'] == ['test']
-    # change content
-    obj.state_int = 42
-    obj.state_list = ['bla']
+    # new object with other content
+    obj = PredefinedAttributes(state_int=42, state_str='new', state_list=['new'])
     # restore content
     obj.set_state(state)
     assert obj.state_list == ['test']
@@ -34,7 +33,7 @@ def test_predefined_default():
 # A derived class may update the attributes
 @dataclass
 class PredefinedAttributesDerived(PredefinedAttributes):
-    state_new_int = 0
+    state_new_int: int = 0
     # best way to extend is to add to the parent attributes setting:
     attributes = PredefinedAttributes.attributes + ['state_new_int']
 
@@ -49,12 +48,74 @@ def test_predefined_derived():
     assert len(state) == 2
     assert state['state_list'] == ['test']
     assert state['state_new_int'] == 0
-    # change content
-    obj.state_int = 42
-    obj.state_list = ['bla']
-    obj.state_new_int = 42
+    # new object with other content
+    obj = PredefinedAttributesDerived(state_int=42, state_str='new', state_list=['new'], state_new_int=15)
     # restore content
     obj.set_state(state)
     assert obj.state_list == ['test']
     assert obj.state_int == 42
     assert obj.state_new_int == 0
+
+
+###############
+## details
+#/
+
+# manual override of attribute and attribute_mask
+def test_manual_get_state():
+    obj = PredefinedAttributes(state_int=42, state_str='predefined', state_list=['predefined'])
+    state = obj.get_state(overwrite_attributes=['state_int', 'state_str', 'state_list'],
+                          additional_attribute_mask=['state_str'])
+    assert 'state_int' in state
+    assert 'state_list' in state
+    assert len(state) == 2
+    assert state['state_int'] == 42
+    assert state['state_list'] == ['predefined']
+
+###############
+## error cases
+#/
+
+# state not being a dict
+def test_state_no_dict():
+    obj = PredefinedAttributes()
+    state = ''
+    with pytest.raises(TypeError) as exc:
+        obj.set_state(state)
+    assert 'uses a dict[str, StateType]' in str(exc.value)
+    assert 'you provided: str' in str(exc.value)
+
+# state including a non-string key
+def test_state_wrong_key_type():
+    obj = PredefinedAttributes()
+    state = {b'state_int': 42}
+    with pytest.raises(TypeError) as exc:
+        obj.set_state(state)
+    assert 'uses a dict[str, StateType]' in str(exc.value)
+    assert "you provided a key: b'state_int'" in str(exc.value)
+    assert 'of type bytes' in str(exc.value)
+
+def test_state_wrong_key_value():
+    obj = PredefinedAttributes()
+    state = {'state_int': PredefinedAttributes()}
+    with pytest.raises(TypeError) as exc:
+        obj.set_state(state)
+    assert 'uses a dict[str, StateType]' in str(exc.value)
+    assert 'you provided a value for key=state_int of type PredefinedAttributes' in str(exc.value)
+
+def test_state_import_wrong_key():
+    obj = PredefinedAttributes()
+    state = {'unsupported_key': 0}
+    with pytest.raises(Warning) as exc:
+        obj.set_state(state)
+    assert 'PredefinedAttributes' in str(exc.value)
+    assert 'includes attribute unsupported_key which is not expected' in str(exc.value)
+    assert "expected are ['state_list']" in str(exc.value)
+
+def test_state_export_wrong_key():
+    class WrongExport(PredefinedAttributes):
+        attributes = ['unsupported_key']
+    obj = WrongExport()
+    with pytest.raises(TypeError) as exc:
+        obj.get_state()
+    assert 'WrongExport' in str(exc.value)
