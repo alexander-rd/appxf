c8a351f1b95d20f6121a62e32f4a20031b5230de
alexander-nbg <mail@alexander-nbg.de>
2025-02-05 10:00:21 +0100

    Review of SettingSelect  + resolved the required fine grained options (mutable_list versus    mutable_items)  + taken select_map out of normal options since they are only    value options dependent on the control options

--- tests/setting/test_setting_select.py
+++ tests/setting/test_setting_select.py
@@ -4,9 +4,12 @@ from kiss_cf.setting import Setting, SettingSelect
 
 def test_init_default():
     setting = SettingSelect(Setting.new(str))
+    # see option definition in SettingSelect for reasons on the expected
+    # values:
     assert setting.options.mutable_items
-    assert not setting.options.custom_value
-    assert setting.get_options() == []
+    assert setting.options.mutable_list
+    assert setting.options.custom_value
+    assert setting.get_select_keys() == []
 
 def test_init_with_items():
     setting = SettingSelect(
@@ -14,7 +17,7 @@ def test_init_with_items():
         value='A',
         select_map={'A': 'One', 'C': 'Three', 'B': 'Two'})
     # obtions are always sorted alphabetically:
-    assert setting.get_options() == ['A', 'B', 'C']
+    assert setting.get_select_keys() == ['A', 'B', 'C']
     assert setting.value == 'One'
 
 def test_add_delete_cycle():
@@ -22,30 +25,31 @@ def test_add_delete_cycle():
         Setting.new(str),
         value='B',
         select_map={'A': 'One', 'B': 'Two', 'D': 'Four'})
-    assert setting.get_options() == ['A', 'B', 'D']
+    assert setting.get_select_keys() == ['A', 'B', 'D']
     assert setting.value == 'Two'
     # adding D:
-    setting.add_option('C', 'Three')
-    assert setting.get_options() == ['A', 'B', 'C', 'D']
+    setting.add_select_item('C', 'Three')
+    assert setting.get_select_keys() == ['A', 'B', 'C', 'D']
     assert setting.value == 'Two'
     # removing B:
-    setting.delete_option('B')
-    assert setting.get_options() == ['A', 'C', 'D']
+    setting.delete_select_key('B')
+    assert setting.get_select_keys() == ['A', 'C', 'D']
     # removing B drops the selection to the next available option (C)
     assert setting.value == 'Three'
     # removing C drops back to D
-    setting.delete_option('C')
-    assert setting.get_options() == ['A', 'D']
+    setting.delete_select_key('C')
+    assert setting.get_select_keys() == ['A', 'D']
     assert setting.value == 'Four'
     # removing D now drops to A:
-    setting.delete_option('D')
-    assert setting.get_options() == ['A']
+    setting.delete_select_key('D')
+    assert setting.get_select_keys() == ['A']
     assert setting.value == 'One'
 
 def test_allowed_values_for_default():
     setting = SettingSelect(
         Setting.new(str),
         select_map={'A': 'One', 'B': 'Two'})
+    setting.options.custom_value = False
     # initial value
     assert setting.value == ''
     assert setting.base_setting.value == ''
@@ -67,8 +71,7 @@ def test_allowed_values_custom_value():
     setting = SettingSelect(
             Setting.new(str, name='base'),
             name='select',
-            select_map={'A': 'One', 'B': 'Two'},
-            custom_value=True)
+            select_map={'A': 'One', 'B': 'Two'})
     setting.value = 'B'
     assert setting.value == 'Two'
     assert setting.base_setting.value == 'Two'
@@ -88,14 +91,13 @@ def test_get_set_cycle():
     data = setting.get_state(value_options=True)
     restored_setting = SettingSelect(Setting.new(str))
     restored_setting.set_state(data)
-    # to ensure the restored select_map is copied over, the following test is
-    # added. But option handling automatically removes stuff from data such
-    # that we only have to check that select_map is not included anymore
-    # instead of tinkering with it:
+    # to ensure the restored select_map is fully copied from data, we overwrite
+    # the corresponding data element with new content
     assert isinstance(data, dict)
-    assert 'select_map' not in data
+    data['select_map']['A'] = 'incorrectA'
+    data['select_map']['B'] = 'incorrectB'
 
-    assert restored_setting.get_options() == ['A', 'B']
+    assert restored_setting.get_select_keys() == ['A', 'B']
     assert restored_setting.value == 'One'
     restored_setting.value = 'B'
     assert restored_setting.value == 'Two'
@@ -114,5 +116,5 @@ def test_get_set_with_custom_value():
     data = setting.get_state(value_options=True)
     restored_setting = SettingSelect(Setting.new(str), custom_value=True)
     restored_setting.set_state(data)
-    assert restored_setting.get_options() == ['A', 'B']
+    assert restored_setting.get_select_keys() == ['A', 'B']
     assert restored_setting.value == 'something'
