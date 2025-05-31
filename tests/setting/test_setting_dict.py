29acd289a637683092dec66cee44a831aa3493d5
alexander-nbg <mail@alexander-nbg.de>
2025-05-31 20:43:01 +0200

    Extending SettingDict test cases for basic Setting behavior and special "overwriting the SettingObject" cases.

--- tests/setting/test_setting_dict.py
+++ tests/setting/test_setting_dict.py
@@ -15,31 +15,26 @@ sample_input = {
     }
 
 init_values = [
-    # Key   Value                                       #Type       Value_Out       String_Out
+    # Key   Value                       Type        Value_Out       String_Out
     # Value based input
-    ('v1',  'test',                                     'string',   'test',         'test'),
-    ('v2',  42,                                         'int',      42,             '42'),
-    ('v3',  1.123,                                      'float',    1.123,          '1.123'),
-    ('v4',  True,                                       'bool',     True,           '1'),
-    # Type based input (resulting in default values)
-#    ('t1',  str,                                        'string',   '',             ''),
-#    ('t2',  int,                                        'int',      0,              '0'),
-#    ('t3',  float,                                      'float',    0.0,            '0.0'),
-#    ('t4',  bool,                                       'bool',     False,          '0'),
+    ('v1',  'test',                     'string',   'test',         'test'),
+    ('v2',  42,                         'int',      42,             '42'),
+    ('v3',  1.123,                      'float',    1.123,          '1.123'),
+    ('v4',  True,                       'bool',     True,           '1'),
     # Setting class based input
-    ('PC1', SettingString,                                 'string',   '',             ''),
-    ('PC2', SettingInt,                                    'int',      0,              '0'),
-    ('PC3', SettingFloat,                                  'float',    0.0,            '0.0'),
-    ('PC4', SettingBool,                                   'bool',     False,          '0'),
+    ('SC1', SettingString,              'string',   '',             ''),
+    ('SC2', SettingInt,                 'int',      0,              '0'),
+    ('SC3', SettingFloat,               'float',    0.0,            '0.0'),
+    ('SC4', SettingBool,                'bool',     False,          '0'),
     # Setting object based input
-    ('p1',  Setting.new(str, 'test'),              'string',   'test',         'test'),
-    ('p2',  Setting.new(int,42),                   'int',      42,             '42'),
-    ('p3',  Setting.new(float,1.123),              'float',    1.123,          '1.123'),
-    ('p4',  Setting.new(bool,True),                'bool',     True,           '1'),
+    ('SO1',  Setting.new(str, 'test'),   'string',   'test',         'test'),
+    ('SO2',  Setting.new(int,42),        'int',      42,             '42'),
+    ('SO3',  Setting.new(float,1.123),   'float',    1.123,          '1.123'),
+    ('SO4',  Setting.new(bool,True),     'bool',     True,           '1'),
     # Tuple based input
-    ('T1',   ('pass', '123abc'),                        'pass',     '123abc',       '123abc'),
-    ('T2',   ('email', 'some@one.com'),                 'email',    '',             ''),
-    ('T3',   ('integer',),                              'int',      0,              '0'),
+    ('T1',  ('pass', '123abc'),         'pass',     '123abc',       '123abc'),
+    ('T2',  ('email', 'some@one.com'),  'email',    '',             ''),
+    ('T3',  ('integer',),               'int',      0,              '0'),
 ]
 
 def _get_setting_reference(t):
@@ -65,6 +60,12 @@ def verify_setting_dict(setting_dict: SettingDict, t_list: list[tuple]):
         assert type(setting_dict.get_setting(t[0])) == type(setting_ref)
         assert setting_dict.get_setting(t[0]).value == setting_ref.value
         assert setting_dict.get_setting(t[0]).to_string() == setting_ref.to_string()
+        # same setting based tests based on value retrieval:
+        assert type(setting_dict.value[t[0]]) == type(setting_ref)
+        assert setting_dict.value[t[0]].value == setting_ref.value
+        assert setting_dict.value[t[0]].to_string() == setting_ref.to_string()
+    # check that value retrieval is same as input retrieval
+    assert setting_dict.value == setting_dict.input
 
 # #############/
 # Init Variants
@@ -86,7 +87,7 @@ def test_setting_dict_init_by_dict():
     verify_setting_dict(setting_dict, init_values)
 
 # general unknown init type
-def test_setting_dict_init_by_tuple_fail3():
+def test_setting_dict_init_fail_by_no_dict():
     # iterable with a iterable subelement that does not contain key+value
     with pytest.raises(AppxfSettingError) as exc_info:
         SettingDict(settings = 42)
@@ -94,23 +95,31 @@ def test_setting_dict_init_by_tuple_fail3():
     assert f'42 of type {int}' in str(exc_info.value)
 
 # inconsistent type/value combinations
-def test_setting_dict_fails_init():
+def test_setting_dict_init_fail_by_invalid_value():
     with pytest.raises(AppxfSettingConversionError):
         SettingDict({
         'test': ('email', 'fail')
         })
 
-def test_setting_dict_tuple_init_fails1():
+def test_setting_dict_init_fail_by_empty_tuple():
     with pytest.raises(AppxfSettingError) as exc_info:
         SettingDict({'test': tuple()})
     assert 'SettingDict' in str(exc_info.value)
     assert '()' in str(exc_info.value)
 
-def test_setting_dict_tuple_init_fails2():
+def test_setting_dict_init_fail_by_too_long_tuple():
     with pytest.raises(AppxfSettingError) as exc_info:
-        SettingDict({'test': (1,2,3,4)})
+        SettingDict({'test': (1,2,3)})
     assert 'SettingDict' in str(exc_info.value)
-    assert '(1, 2, 3, 4)' in str(exc_info.value)
+    assert '(1, 2, 3)' in str(exc_info.value)
+
+def test_setting_dict_init_fail_by_no_str_key():
+    with pytest.raises(AppxfSettingError) as exc_info:
+        SettingDict({13: SettingInt(42)})
+    assert 'SettingDict' in str(exc_info.value)
+    assert 'Cannot set key 13' in str(exc_info.value)
+    assert 'Only string keys are supported' in str(exc_info.value)
+
 
 # ####################/
 # Value Manipulations
@@ -130,6 +139,53 @@ def test_setting_dict_set_invalid_value():
         })
     with pytest.raises(AppxfSettingConversionError):
         setting_dict['test'] = 'fail'
+    # error message checks are in scope of setting implementation
+
+def test_setting_dict_overwriting_by_setting_object():
+    setting_dict = SettingDict({
+        'test': ('bool', False)})
+    assert setting_dict['test'] == False
+    assert type(setting_dict.value['test']) == SettingBool
+    # overwrite by Email
+    setting_dict['test'] = Setting.new('string', 'some')
+    assert setting_dict['test'] == 'some'
+    assert type(setting_dict.value['test']) == SettingString
+
+def test_setting_dict_overwriting_by_setting_class():
+    setting_dict = SettingDict({
+        'test': ('bool', False)})
+    assert setting_dict['test'] == False
+    assert type(setting_dict.value['test']) == SettingBool
+    # overwrite by Email
+    setting_dict['test'] = SettingInt
+    assert setting_dict['test'] == SettingInt.get_default()
+    assert type(setting_dict.value['test']) == SettingInt
+
+def test_setting_dict_overwriting_by_tuple():
+    setting_dict = SettingDict({
+        'test': ('bool', False)})
+    assert setting_dict['test'] == False
+    assert type(setting_dict.value['test']) == SettingBool
+    # overwrite by Email
+    setting_dict['test'] = ('string', 'some')
+    assert setting_dict['test'] == 'some'
+    assert type(setting_dict.value['test']) == SettingString
+
+def test_setting_dict_keeps_setting_object():
+    setting_dict = SettingDict({
+        'test': (str, 'init')})
+    assert setting_dict['test'] == 'init'
+    init_object = setting_dict.value['test']
+    # set new value
+    setting_dict['test'] = 'new'
+    assert setting_dict['test'] == 'new'
+    new_object = setting_dict.value['test']
+    assert new_object is init_object
+    # set overwriting variant
+    setting_dict['test'] = (str, 'overwriting')
+    assert setting_dict['test'] == 'overwriting'
+    new_object = setting_dict.value['test']
+    assert new_object is not init_object
 
 # setting nonexisting is now expected to work:
 #def test_setting_dict_set_nonexisting():
