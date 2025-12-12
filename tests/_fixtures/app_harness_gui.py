2559edbfc93028f65065c28bb64f0f5033c8df4a
alexander-nbg <mail@alexander-nbg.de>
2025-12-12 11:29:45 +0100

    Further drafting the registration GUI.  * Request can be generated and read in (with user data being displayed)  * User can be added and response generated Before adding the reading of the response, adding the user needs to be adjusted since currently, the user can be added multiple times. Also, it shall be possible to generate a response for an already registered user and the GUI shall display whether the user is already registered.

--- tests/_fixtures/app_harness_gui.py
+++ tests/_fixtures/app_harness_gui.py
@@ -1,5 +1,6 @@
 ''' Provide a GUI for the application harness '''
-from kiss_cf.gui import KissApplication, ConfigMenu, Login, Registration
+from kiss_cf.gui import KissApplication, ConfigMenu, Login
+from kiss_cf.gui import RegistrationUser, RegistrationAdmin
 from tests._fixtures.app_harness import AppHarness
 import tkinter
 
@@ -37,35 +38,18 @@ class AppHarnessGui():
 
         # --- Registration Menu --- #
         if self.harness.registry_enabled and self.harness.registry.is_initialized():
-            registration = Registration(registry=self.harness.registry)
+            def show_registration():
+                registration = RegistrationAdmin(
+                    registry=self.harness.registry,
+                    root_dir=self.harness.root_path,
+                    user_config=self.harness.config.section('USER'))
+                registration.show()
             app.frame_menu.add_command(
                 label='Registration',
-                command=lambda: registration.register_user())
+                command=show_registration)
 
-        # Problem: the base64 encoded encryption key has this length, the line
-        # length is from what a reasonably sized pop-up window can display.
-        # Those are 55 characters per line, 392 characters. Those are 294 bytes
-        # (due to DER encoding). The key is 2048 bit and 256 bytes such that
-        # there is no significant gain.
-        #
-        # MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuS0eHWUOg2N
-        # DM8w7wsYF4/cJl/dpoB+Ru3zVVr77joFpojfNmFzwXnQnOyWwUgoX4k
-        # kFjELXzOO9FZ7i830ua6y0jvZqaU1Cvlamtvs1slH+62pDKHFDnTifo
-        # 7O9goFSmHHLi2GJ8aoRgEfb/4wJVdN8B6iA8bctWKxduEBZiCfF03rI
-        # EyHJaRjsqLZpYBC7jri6OCWlxU+8kEZyvpSgwyB2hwfGE1lG5K8MR/R
-        # LA7ANi0YUcZ4VqGSQGyYM0LalupX4w2UXJjxBT17UblTUa/+HEKHsvx
-        # DyO5CDtbYUZkjwXXgoJ3jt76gcazoFYikG+VjhK0V8cU5vfVWYU6lB8
-        # wIDAQAB------------------------------------------------
-        #
-        # Conlcusion: key should also be exchanged by a file. If it will be per
-        # file, we can as well send ALL admin keys.
 
-        # --- Adding more menu items --- #
-        #topMenu = tkinter.Menu(self)
-        #self.config(menu=topMenu)
-        #topMenu.add_command(label="Item A", command=lambda: print('Menu: item A'))
-        #topMenu.add_command(label="Item B", command=lambda: print('Menu: item B'))
-        # self.update()
+
 
         app.mainloop()
 
@@ -84,9 +68,11 @@ class AppHarnessGui():
 
         # ensure registry being initialized
         if self.harness.registry_enabled and not self.harness.registry.is_initialized():
-            registration = Registration(
-                registry = self.harness.registry)
-            registration.check()
+            registration = RegistrationUser(
+                registry = self.harness.registry,
+                root_dir = self.harness.root_path)
+            if not registration.check():
+                return
 
         # loading regitry will happen automatically when required - like during
         # a sync attempt:
