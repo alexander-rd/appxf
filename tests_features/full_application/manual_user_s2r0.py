81b5c2cf3064a91e25042bb0e2b1423d88e85cbe
alexander-nbg <mail@alexander-nbg.de>
2026-01-21 11:19:32 +0100

    German translation visible in manual_user_s2r0 How to apply needs improvement as well as GUI optics.

--- tests_features/full_application/manual_user_s2r0.py
+++ tests_features/full_application/manual_user_s2r0.py
@@ -9,6 +9,7 @@ __Reset:__ The sandbox is NOT reset during executions to support development
 testing. Use the reset button to manualle reset the sandbox. **Do not reset
 while the application is launched!**
 '''
+import locale
 from kiss_cf.storage import Storage
 
 from appxf_matema.case_runner import ManualCaseRunner
@@ -32,6 +33,17 @@ def process_reset_sandbox():
 
 def process_app_user():
     ''' Launch User '''
+    # Set locale to German for testing translations - execution SHALL fail if
+    # loading is not possible
+    locale.setlocale(locale.LC_ALL, 'de_DE.UTF-8')
+    # Load German translations
+    import gettext
+    trans = gettext.translation('appxf-gui', localedir='locale', languages=['de'], fallback=True)
+    trans.install()
+    
+    # Debug: check if translation is loaded
+    print(f"Translated 'Admin Keys': {_('Admin Keys')}")
+
     sandbox_path = test_sandbox.init_test_sandbox_for_caller_module(cleanup=False)
     app_user = AppHarness(sandbox_path, 'user',
                           registry_enabled=True)
