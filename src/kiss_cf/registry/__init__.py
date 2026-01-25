d95c8326aa389eb25a674c3352009540d481dd89
alexander-nbg <mail@alexander-nbg.de>
2026-01-25 17:30:34 +0100

    Block non-admins from getting manual updates

--- src/kiss_cf/registry/__init__.py
+++ src/kiss_cf/registry/__init__.py
@@ -7,6 +7,6 @@ secure Storages from the storage module.
 
 # flake8: noqa F401
 
-from .registry import Registry, KissRegistryError
+from .registry import Registry, AppxfRegistryError, AppxfRegistryRoleError
 from .shared_storage import SecureSharedStorage
 from .shared_sync import SharedSync
