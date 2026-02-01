82590dc191f5b51ef965f46bd84d00737651d82f
alexander-nbg <mail@alexander-nbg.de>
2026-02-01 16:01:29 +0100

    Further review and refactoring  * adding to flake8 configuration into pyproject.toml

--- src/appxf_private/__init__.py
+++ src/appxf_private/__init__.py
@@ -1,6 +1,9 @@
-# flake8: noqa F401
+# Facade for private APPXF basic classes
+#
+# Basic classes exposed here shall only have dependencies to python builtin
+# modules. This facade shall not expose any object from sub-modules. Rationale:
+# using APPXF shall not enforce loading of unnecessary dependencies which are
+# typically present in sub-modules.
+
 from .stateful import Stateful
 from .options import Options
-
-# module contents are not forwarded on this base layer: they would enforce
-# every user to load them even if not required
