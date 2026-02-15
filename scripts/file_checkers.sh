07bb217a82cd4a3725372f91a1fc6ba07ebac89e
alexander-rd <mail@alexander-rd.de>
2026-02-15 15:56:15 +0100

    Initial version of the file checker

---
+++ scripts/file_checkers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Copyright 2026 the contributors of APPXF (github.com/alexander-rd/appxf)
+# SPDX-License-Identifier: 0BSD
+
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPO_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
+
+export PYTHONPATH="$REPO_ROOT/src${PYTHONPATH:+:$PYTHONPATH}"
+
+python3 -m appxf_dev.file_checkers "$@"
